a9c935aa18e6910e589384fd8a7c0688051983d1
Lukasz Grela <LGrela@omnicogroup.com>
2021-04-17 13:05:23 +0100

    navigation links

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { Paths } from "../AppRouter/AppRouter";
+import { pathBuilder } from "../AppRouter/utils";
 
 const Navigation: React.FC = (): JSX.Element => {
-  return <nav></nav>;
+  return (
+    <div className="Navigation">
+      <nav>
+        <span className="Navigation_logo">
+          <Link to={pathBuilder(Paths.HOME, [{ key: "tag" }], true)}>
+            Hacker News
+          </Link>
+        </span>
+        <NavLink to={pathBuilder(Paths.HOME, [{ key: "tag", value: "new" }])}>
+          new
+        </NavLink>
+        <NavLink to={pathBuilder(Paths.HOME, [{ key: "tag", value: "show" }])}>
+          show
+        </NavLink>
+        <NavLink to={pathBuilder(Paths.HOME, [{ key: "tag", value: "ask" }])}>
+          ask
+        </NavLink>
+        <NavLink to={pathBuilder(Paths.HOME, [{ key: "tag", value: "jobs" }])}>
+          jobs
+        </NavLink>
+      </nav>
+    </div>
+  );
 };
 
 export default Navigation;
