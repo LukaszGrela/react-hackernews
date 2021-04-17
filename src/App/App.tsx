424a92a1483c5f63163457b71ed5e9a6c6d38305
Lukasz Grela <LGrela@omnicogroup.com>
2021-04-17 13:06:11 +0100

    Change App structure

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,19 +1,14 @@
 import React from "react";
-import Navigation from "../components/Navigation/Navigation";
 import { Provider } from "react-redux";
 import store from "../store";
-import "./styles/index.scss";
 import AppRouter from "../components/AppRouter/AppRouter";
+
+import "./styles/index.scss";
 
 const App: React.FC = (): JSX.Element => {
   return (
     <Provider store={store}>
-      <div className="App">
-        <header className="App-header">
-          <Navigation />
-        </header>
-        <AppRouter />
-      </div>
+      <AppRouter />
     </Provider>
   );
 };
